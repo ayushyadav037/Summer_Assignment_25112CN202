e67166731238e21c3df7531cc4206093119b225b
anshu yadav <ayushyadavmpi03@gmail.com>
2026-06-01 17:43:42 +0530

    Day-1 uplodaded.

--- Day-1/1_To_calculate_sum_of_first_N_natural_number_.c
+++ Day-1/1_To_calculate_sum_of_first_N_natural_number_.c
@@ -4,7 +4,7 @@ int main()
     int n , sum ;
     printf("enter a positive integer : ");
     scanf("%d",&n);
-    printf("sum of N natural number : %d",sum=n*(n+1)/2);
+    printf("sum of N natural number : %d",sum=(n*(n+1))/2);
     return 0;
 
 }
